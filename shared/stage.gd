3d6d8480659131e7628223de5dfa3841848269dd
Jamis Charles <jamis82@webutah.com>
2023-10-05 09:37:28 -0600

    clicking on single shape creates match and disables RR button

--- shared/stage.gd
+++ shared/stage.gd
@@ -32,6 +32,7 @@ var canvas_click_circle: Polygon2D
 @onready var legend_button_group: ButtonGroup = $HBoxContainer/right_rail.get("legend_button_group")
 @onready var clickZoneContainer = $HBoxContainer/ScrollContainer/HBoxContainer/CanvasContainer/click_zone_container
 
+@onready var right_rail_buttons: Array[Node] = $HBoxContainer/right_rail/legend_for_hidden_objects.get_children()
 
 # Simplest shape solution...
 # Draw a few polygons that have the 3-4 shapes I care about 
@@ -274,10 +275,6 @@ func _ready():
 		else:
 			assert_verify_matches[btn_name] = 1
 			
-	# verify there's a match for each (exactly count of 2 now)
-#	for zone in assert_verify_matches:
-#		assert(assert_verify_matches[zone] == 2, "ClickZone/legendBtn mismatch: %s has %s" % [zone, assert_verify_matches[zone]])
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -285,144 +282,63 @@ func _process(delta):
 	pass
 
 
-# Any time we click on a shape, or a right rail button we want to compare for a match
-func compare_for_match(selected_object1, selected_object2):
-	print('eval for match: ', selected_object1, selected_object2)
-	
-	# ensure we have 2 objects selected we can compare
-	if !selected_object1 || !selected_object2: return
-
-	
-#	get which animal button is pressed
-	
-	# get selected right rail animal
-	
-	print("COMPARE:", selected_object1, selected_object2)
-	
-	
-	if selected_object1.name == selected_object2.name:	
-		## disable and color canvas collision shape click_zone
-		on_match_found(selected_object1, selected_object2)
-		
-		
-	
-	
-	
-	# get current active click target on canvas (with circle)
-
-# when a shape on the canvas is clicked, run the comparison
-#func on_shape_clicked(shape_name: String, shape: Area2D):
-#	selected_animal = shape
-#	compare_for_match(shape, animalsBtnGroup.get_pressed_button())
-	
-		
-#whenever the right rail selection changes it'll get updated in selected_object2
-func on_button_group_change():
-	print('button group changed!!')
-	selected_obj_ref2 = legend_button_group.get_pressed_button()
-	
-	## TODO: Simplify and reuse
-	
-	for shape in selected_canvas_shapes_ref:
-		compare_for_match(shape, selected_obj_ref2)
-#	print('selectedAnimal2: ', selected_animal2)
 
 #whenever overlapping shapes are found in canvas pass array of overlapping shapes
 func on_canvas_shapes_found(shapes: Array[Area2D]):
 	selected_canvas_shapes_ref = shapes
 	# compare each overlapping shape passed in (from click circle)
 	for shape in shapes:
-		compare_for_match(shape, legend_button_group.get_pressed_button())
+		on_shape_found(shape)
 
 # on match found
-func on_match_found(animal1, obj2: Button):
+func on_shape_found(shape):
 #	var firstNode = instance_from_id(firstUid)
 	
 	# canvas click zone. Gray out, then disable
-	animal1.set_modulate(Color(.36, .36, .36, .74))
+	shape.set_modulate(Color(.36, .36, .36, .74))
 	# functionally - disable clicks
-	animal1.set_pickable(false) # bubbles up from collisionlayer. Nice!
+	shape.set_pickable(false) # bubbles up from collisionlayer. Nice!
 	
+	print('shape.name##', shape.get_name())
+	var rr_btn = find_matching_right_rail_button(shape.get_name())
 	
 	## Right rail button. Disable and fade out
-	obj2.disabled = true
+	rr_btn.disabled = true
 	# stop using the shader, so we can effect the alpha channel of this
-	obj2.use_parent_material = true
-	obj2.set_modulate(Color(1,1,1,.5))
-	obj2.get_parent().move_child(obj2, obj2.get_parent().get_child_count()) # move to end
-	
-#	obj2.icon.set_modulate(Color(1,1,1,.2))
-#	obj2.icon.modulate = Color(1,1,1,.2)
-
-
-	
-	
-#	 $Button.modulate = Color(0.5,0.5,0.5,1)
-#  $$Button/sprite_itemIcon.modulate = Color(0.25,0.25,0.25)
+	rr_btn.use_parent_material = true
+	rr_btn.set_modulate(Color(1,1,1,.5))
+	rr_btn.get_parent().move_child(rr_btn, rr_btn.get_parent().get_child_count()) # move to end
 	
 	
 	# Verify we have no more sets to match
 	# ie: Is the level done?
-#	current_score += 1
 #	$HUD/score_label.set_text("%s / %s" % [current_score, total_match_sets])
-	var right_rail_buttons = $HBoxContainer/right_rail/legend_for_hidden_objects.get_children()
+	
 	for btn in right_rail_buttons:
 		print("name", btn.name)
 		if !btn.is_disabled():
 			return
 			
 	end_level()
-#	for i in len(right_rail_buttons):
-#		var btn = right_rail_buttons[i]
-	
-	
 
-		# destroy the old one, create the new...
-		# or do we just emulate and have the parent draw?
-#		Yes. let's do that...
+## Pass in the name of the shape(node) then returns that right rail button by that name
+func find_matching_right_rail_button(shape_name:String)->Button:
+	var searchTerm = Button.new()
+	searchTerm.name = shape_name
+#	var result = right_rail_buttons.find(searchTerm)
+	# find is weird. so using filter instead
 	
+	## FIXME: error handling?!? This could explode
+	return right_rail_buttons.filter(func(x): return x.name == shape_name)[0]
+
 	
 	
 func end_level():
 	$level_complete_panel.visible = true # show the 
 	
-#func destroy_and_spawn_click_circle(x:float,y:float):
-##	
-#	# destroy the old circle if it exists
-#	if canvas_click_circle: canvas_click_circle.queue_free()
-#
-#	print('destroy: ', x,y)
-#	var clickCircle: Area2D = Area2D.new()
-#	var collision: CollisionShape2D = CollisionShape2D.new()
-#	var shape: CircleShape2D = CircleShape2D.new()
-#	shape.radius = 70
-#
-#
-#
-#	clickCircle.add_child(collision)
-##	collision.shape.set("shape", "CircleShape2D")
-#	collision.shape = shape
 
-	# circle polygon
-#	_draw(x,y)
-	
-	# TODO: Add tweening
 	
 	
-	
-#func _on_click_zone_input_event(viewport, event, shape_idx):
-##	https://docs.godotengine.org/en/4.1/tutorials/inputs/inputevent.html
-#	if event is InputEvent:
-#		if event.is_pressed():
-#			print("event", event)
-#			print('shape# ', shape_idx)
-#			print('SKUNK')
-#	if event.pressed: 
-#		print("view", viewport)
-	
-#		print("shape_idx", shape_idx)
-#		print('click!')
-	# Q: Can we use the ID to tie the source and matched animal together?
 
 # UTILS ---------------
 
